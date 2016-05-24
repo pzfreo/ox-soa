3f8e0188793a9cd66d1929959a90020c720fa546
Paul Fremantle <pzfreo@github.com>
2016-05-24 13:22:52 +0100

    blah

--- code/before/ex1/simplehttp_spec.js
+++ code/before/ex1/simplehttp_spec.js
@@ -3,12 +3,12 @@ var frisby = require('frisby');
 frisby.create('Test Random Number service')
   .get('http://localhost:8080/')
   .expectStatus(200)
-  .expectHeaderContains('content-type', 'application/json')
-  .expectJSON({
-    random: function(v) { expect(v).toBeGreaterThan(0);expect(v).toBeLessThan(51);}
-  })
+  .expectHeaderContains('Content-Type', 'application/json')
   .expectJSONTypes( {
     random: Number
     }
   )
+  .expectJSON({
+    random: function(v) { expect(v).toBeGreaterThan(0);expect(v).toBeLessThan(101);}
+  })
 .toss();
