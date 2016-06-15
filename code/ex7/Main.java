02466aaabdbd3ffd1d7ad9f58d19b47e331569bb
Paul Fremantle <pzfreo@github.com>
2016-06-15 11:37:16 +0100

    update

--- code/ex7/Main.java
+++ code/ex7/Main.java
@@ -2,42 +2,47 @@ package freo.me.rest;
 
 import java.net.URI;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.UriBuilder;
 
 import org.eclipse.jetty.server.Server;
 import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
+@ApplicationPath("/")
 public class Main extends ResourceConfig {
 
-	public Main() {
-		packages("freo.me.rest");
+
+public Main() {
+	// this is the package to scan for Resources
+	packages("freo.me.rest");
+}
+
+public static void main(String[] args) throws Exception {
+	URI baseUri = UriBuilder.fromUri("http://localhost/").port(8080)
+			.build();
+	// This is fairly self-explanatory.
+	// You can define the URL on which the server will listen.
+
+	ResourceConfig config = new Main();
+	// This is where we identify that the class POResource is the JAX-RS
+	// Resource (aka Service) that we want to expose.
+
+	Server server = JettyHttpContainerFactory.createServer(baseUri, config);
+	// Here is where we create the Jetty Server object.
+
+	try {
+		server.start();
+		// This initiates the startup of the server.
+
+		server.join();
+		// Wait until the server finishes initiation
+
+	} finally {
+		server.destroy();
+		// Obvious!
 	}
-	
-	public static void main(String[] args) throws Exception {
-		URI baseUri = UriBuilder.fromUri("http://localhost/").port(8080).build();
-		// This is fairly self-explanatory.
-		// You can define the URL on which the server will listen.
 
-		ResourceConfig config = new Main();
-		// This is where we identify that the class POResource is the JAX-RS
-		// Resource (aka Service) that we want to expose.
-
-		Server server = JettyHttpContainerFactory.createServer(baseUri, config);
-		// Here is where we create the Jetty Server object.
-
-		try {
-			server.start();
-			// This initiates the startup of the server.
-
-			server.join();
-			// Wait until the server finishes initiation
-
-		} finally {
-			server.destroy();
-			// Obvious!
-		}
-
-	}
+}
 
 }
