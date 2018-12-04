ee79ea8b5e868d5a4006da02bc3ec27dde405b93
Paul Fremantle <pzfreo@github.com>
2018-12-04 09:50:09 +0000

    .

--- code/ex1/index.spec.js
+++ code/ex1/index.spec.js
@@ -3,13 +3,13 @@ const Joi = frisby.Joi;
 
 it('test random number service', function(done) {
   frisby.get('http://localhost:8080/')
-  .expect('status',201)
+  .expect('status', 201)
   .expect('header', 'Content-Type', 'application/json; charset=utf-8')
-  .expect('jsonTypes', '/',  { random: Joi.number() })
+  .expect('jsonTypes', 'random', Joi.number().required() )
 	.then(function(res) {
 		expect(res.json.random).toBeGreaterThan(1);
 		expect(res.json.random).toBeLessThan(100);
 		}
-	).done(done)
-
+	)
+	.done(done)
 });
