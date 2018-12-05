1173c0f785af3c50fe4fe9bb03dca095024986a6
Paul Fremantle <pzfreo@github.com>
2018-12-05 12:24:40 +0000

    .

---
+++ code/python-rabbit/subscriber.py
@@ -0,0 +1,16 @@
+import pika
+import json
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='purchase')
+
+def callback(ch, method, properties, body):
+    result = json.loads(body)
+    print(result)
+
+channel.basic_consume(callback,
+                      queue='purchase',
+                      no_ack=True)
+
+channel.start_consuming()
