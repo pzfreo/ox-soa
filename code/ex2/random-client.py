e0a8398d093508ebdf76eba8d07e067a0745abad
Paul Fremantle <pzfreo@github.com>
2019-11-18 08:56:23 +0000

    pyth3

--- code/ex2/random-client.py
+++ code/ex2/random-client.py
@@ -6,6 +6,6 @@ url = "http://localhost:8080"
 h = httplib2.Http()
 resp, content = h.request(url, "GET")
 
-print "return code: " + resp['status']
+print ("return code: " + resp['status'])
 result = json.loads(content)
-print "random number: " + str(result['random'])
+print ("random number: " + str(result['random']))
