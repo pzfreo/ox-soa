210ffbef2afac596ebd551f6311041ff15e1b8bd
Paul Fremantle <pzfreo@github.com>
2021-04-09 21:59:56 +0100

    updated slides and docs

--- code/gdocs.sh
+++ code/gdocs.sh
@@ -3,6 +3,7 @@ rm ~/repos/ox-soa/lab-source/*.docx
 rm ~/repos/ox-soa/lab-pdfs/*.pdf
 rm ~/repos/ox-soa/presentation-source/*.pptx
 rm ~/repos/ox-soa/presentation-pdfs/*.pdf
+drive pull
 drive pull -no-prompt -explicitly-export -export docx -exports-dir ~/repos/ox-soa/lab-source/ -same-exports-dir ~/drive/ox-soa/lab-source/ 
 drive pull -no-prompt -explicitly-export -export pdf -exports-dir ~/repos/ox-soa/lab-pdfs/ -same-exports-dir ~/drive/ox-soa/lab-source/ 
 drive pull -no-prompt -explicitly-export -export pptx -exports-dir ~/repos/ox-soa/presentation-source/ -same-exports-dir ~/drive/ox-soa/presentation-source/ 
