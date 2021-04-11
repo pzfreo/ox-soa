ef45c20e0afb4504aaead73cfc3b0c2e2d0eec9d
Paul Fremantle <pzfreo@github.com>
2021-04-11 10:03:56 +0100

    .

---
+++ code/sec/snippet.ts
@@ -0,0 +1,22 @@
+const port = process.env.PORT || 8443;
+
+const base_dir = "/home/oxsoa/sec/server/keys";
+
+const secureServer = createServer({
+  key: readFileSync(base_dir+'/private/server.key.pem'),
+  cert: readFileSync(base_dir+'/server.cert.pem'),
+  ca: readFileSync(base_dir+'/ca.cert.pem'),
+  requestCert: true,
+  passphrase: "password",
+
+  rejectUnauthorized: false
+}, app);
+
+secureServer.listen(port, function() {
+  console.log(`Secure Purchase app listening at https://localhost:${port}`);
+});
+
+
+// app.listen(port, () =>
+//   console.log(`Purchase app listening at http://localhost:${port}`)
+// );
